05c51d93cc286d7490b6b973434e8cd7dbeb17f5
Fuzzkatt <zonghan2000@gmail.com>
2017-01-22 21:59:36 -0800

    Add files via upload

---
+++ KOPDriveTrain/src/org/usfirst/frc/team5905/subsystems/DriveTrain.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team5905.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team5905.robot.Robot;
+import org.usfirst.frc.team5905.robot.RobotMap;
+import org.usfirst.frc.team5905.robot.commands.*;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	private enum DriveTrainMode {
+		MANUAL, DRIVE_STRAIGHT, ROTATE, DRIVE_BACKWARDS
+	}
+	private final RobotDrive myDrive = RobotMap.drive;
+	DriveTrainMode mode;
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new MoveWithJoysticks());
+    }
+    
+    public void prepareManual(){
+    	mode = DriveTrainMode.MANUAL;
+    }
+    
+    public void moveWithJoysticks(){
+    	double leftPower = -1 * RobotMap.robotSpeed * Robot.oi.gamepad.getRawAxis(RobotMap.LEFT_GAMEPAD_JOYSTICK_Y);
+		double rightPower = -1 * RobotMap.robotSpeed * Robot.oi.gamepad.getRawAxis(RobotMap.RIGHT_GAMEPAD_JOYSTICK_Y);
+		myDrive.tankDrive(leftPower,  rightPower);
+    }
+    
+    public void reverse(){
+    	myDrive.setInvertedMotor(MotorType.kFrontLeft, true);
+    }
+}
+
---
+++ KOPDriveTrain/src/org/usfirst/frc/team5905/subsystems/Intake.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team5905.robot.subsystems;
+
+import org.usfirst.frc.team5905.robot.RobotMap;
+import org.usfirst.frc.team5905.robot.commands.*;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Intake extends Subsystem {
+
+    public void initDefaultCommand() {
+        //no default so when not toggled, SpinInward doesn't run
+    	
+    }
+    
+    public void spin(int direction, double speed) {
+		RobotMap.ARM_LEFT_SPEED_CONTROLLER.set(speed * direction);
+		RobotMap.ARM_RIGHT_SPEED_CONTROLLER.set(-1 * speed * direction);
+	}
+
+	public void spinInward() {
+		spin(1, RobotMap.INTAKE_SPEED);	
+	}
+	
+	public void stopSpin() {
+		spin(0, 0);
+	}
+}
+
