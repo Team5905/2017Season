8c628393e718aa18f7f951452ef3e51fceda0f30
Patrick Wang <zonghan2000@gmail.com>
2017-02-03 19:16:08 -0800

    intake wasn't updated apparently

--- KOPDriveTrain/src/org/usfirst/frc/team5905/subsystems/Intake.java
+++ KOPDriveTrain/src/org/usfirst/frc/team5905/subsystems/Intake.java
@@ -1,28 +1,25 @@
 package org.usfirst.frc.team5905.robot.subsystems;
 
 import org.usfirst.frc.team5905.robot.RobotMap;
-import org.usfirst.frc.team5905.robot.commands.*;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Intake extends Subsystem {
-
+    
     public void initDefaultCommand() {
         //no default so when not toggled, SpinInward doesn't run
-    	
+        
     }
     
-    public void spin(int direction, double speed) {
-		RobotMap.ARM_LEFT_SPEED_CONTROLLER.set(speed * direction);
-		RobotMap.ARM_RIGHT_SPEED_CONTROLLER.set(-1 * speed * direction);
-	}
-
-	public void spinInward() {
-		spin(1, RobotMap.INTAKE_SPEED);	
-	}
-	
-	public void stopSpin() {
-		spin(0, 0);
-	}
+    public void spin() {
+        double speed = RobotMap.INTAKE_SPEED;
+        RobotMap.ARM_LEFT_SPEED_CONTROLLER.set(speed);
+        RobotMap.ARM_RIGHT_SPEED_CONTROLLER.set(-1 * speed);
+    }
+    
+    public void stopSpin() {
+        RobotMap.ARM_LEFT_SPEED_CONTROLLER.stopMotor();
+        RobotMap.ARM_RIGHT_SPEED_CONTROLLER.stopMotor();
+    }
 }
 
