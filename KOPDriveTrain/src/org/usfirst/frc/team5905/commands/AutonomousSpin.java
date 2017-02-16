415c617379cde41289fec21a9faf1c1e3a3e4f58
Cashmuney <nikhil0929@gmail.com>
2017-02-15 21:06:48 -0800

    Add files via upload

---
+++ KOPDriveTrain/src/org/usfirst/frc/team5905/commands/AutonomousSpin.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team5905.robot.commands;
+
+import org.usfirst.frc.team5905.robot.*;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team5905.robot.*;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team5905.robot.*;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team5905.robot.RobotMap;
+public class AutonomousSpin extends Command {
+
+    double timer = 0;
+    int theta = 150;
+    double maxtime = 0;
+    boolean exit = false;
+
+    public AutonomousSpin() {
+     requires(Robot.driveTrain);
+    }
+
+    protected void initialize() {
+     maxtime = .01 * theta/RobotMap.TURN_SPEED;
+     timer = 0;
+
+    }
+
+    protected void execute() {
+
+        Robot.driveTrain.spin();
+        timer+=.02;
+        if (timer >= maxtime)
+            exit = true;
+    }
+
+    protected boolean isFinished() {
+        return exit;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}
+
